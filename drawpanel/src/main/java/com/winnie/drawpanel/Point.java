b434a8e1483ce281a74048d0410413e641846741
Winnie <yeonsu100@gmail.com>
2020-02-21 15:17:13 +0900

    step03

--- drawpanel/src/main/java/com/winnie/drawpanel/Point.java
+++ drawpanel/src/main/java/com/winnie/drawpanel/Point.java
@@ -2,7 +2,7 @@ package com.winnie.drawpanel;
 
 import java.io.Serializable;
 
-public class Point implements Serializable {        // 객체를 파일에 저장, 읽어들이기 위해 serializable한다.
+public class Point implements Serializable {    // 객체를 파일에 저장, 읽어들이기 위해 serializable한다.
     public int x;
     public int y;
     public boolean isStartPoint;

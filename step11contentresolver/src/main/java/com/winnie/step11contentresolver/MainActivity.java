81756c237db0c458252e309907a6a1e35a33350a
Winnie <yeonsu100@gmail.com>
2020-02-26 11:58:25 +0900

    step11 - Call

--- step11contentresolver/src/main/java/com/winnie/step11contentresolver/MainActivity.java
+++ step11contentresolver/src/main/java/com/winnie/step11contentresolver/MainActivity.java
@@ -7,12 +7,14 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,12 +24,15 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity
+                        implements View.OnClickListener,
+                                    AdapterView.OnItemClickListener {
     // 필요한 필드 정의하기
     private EditText editText;
     private ListView listView;
     private ContactAdapter adapter;
     private List<ContactDto> list=new ArrayList<>();
+    private int selectedIndex;          // 클릭한 셀의 인덱스 값을 저장할 필드
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +46,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 버튼의 참조값 얻어와서 리스너 등록하기
         Button findBtn=findViewById(R.id.findBtn);
         findBtn.setOnClickListener(this);
+
+        // ListView에 아이템 클릭 리스너 등록하기
+        listView.setOnItemClickListener(this);
     }
 
     // 연락처 정보를 가져오는 메소드
@@ -118,6 +126,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             Toast.LENGTH_LONG).show();
                 }
                 break;
+            case 1:                 // 1 번 요청 코드인 경우
+                // 퍼미션을 Allow 했을경우
+                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    call();
+                }else{              // Allow 하지 않았을 경우
+                    Toast.makeText(this, "Please check the permission state!",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
         }
+    }
+
+    // ListView의 cell을 클릭하면 호출하는 메소드
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        // 선택한 인덱스 값을 필드에 저장
+        selectedIndex=i;
+
+        int permissionCheck=
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.CALL_PHONE);
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED){       // 권한이 없다면
+            // 권한을 얻어야하는 퍼미션 목록
+            String[] permissions={Manifest.permission.CALL_PHONE};
+            // 권한을 얻어내도록 유도 (이 때 요청 코드는 0이 아닌 1로 함으로써 READ_CONTACTS와 다르게 해야한다.)
+            ActivityCompat.requestPermissions(this, permissions, 1);
+        }else{                      // 권한이 있다면
+            call();
+        }
+    }
+
+    // 전화를 거는 메소드
+    public void call(){
+        // 어댑터로부터 i번째 아이템을 얻어온다.
+        ContactDto dto=(ContactDto) adapter.getItem(selectedIndex);
+        // 전화번호를 얻어낸다.
+        String phone=dto.getPhone();
+        // 전화를 걸겠다는 Intent 객체를 생성한다.
+        Intent intent=new Intent();
+        /*
+            - Intent.ACTION_DIAL은 통화버튼을 눌러야 전화가 걸리므로 permission이 필요없다.
+            - Intent.ACTION_CALL은 바로 전화가 걸리기 때문에 permission을 얻어야한다.
+         */
+        //intent.setAction(Intent.ACTION_DIAL);
+        intent.setAction(Intent.ACTION_CALL);
+        // 전화번호를 Uri로 전달한다.
+        Uri uri=Uri.parse("Tel : "+phone);
+        intent.setData(uri);
+        // 해당 인텐트를 받아줄 수 있는 액티비티를 찾는다.
+        startActivity(intent);
     }
 }

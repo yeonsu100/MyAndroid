acbcd79b7f529f43e3a4876614f97c862025710c
Winnie <yeonsu100@gmail.com>
2020-02-26 16:03:54 +0900

    toy01 (not finished)

---
+++ toy01/src/main/java/com/winnie/toy01/MainActivity.java
@@ -0,0 +1,17 @@
+package com.winnie.toy01;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends View {
+    public MainActivity(Context context) {
+        super(context);
+    }
+
+    public MainActivity(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}

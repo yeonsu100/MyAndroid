18c6f03116dcfb35e45e170f231b8305b6a17bd2
Winnie <yeonsu100@gmail.com>
2020-02-27 13:16:58 +0900

    step12 (building...)

--- step12sharedpref/src/main/java/com/winnie/step12sharedpref/MainActivity.java
+++ step12sharedpref/src/main/java/com/winnie/step12sharedpref/MainActivity.java
@@ -1,19 +1,31 @@
 package com.winnie.step12sharedpref;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+                        implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+    // 필요한 필드
+    private EditText editText;
+    private Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +42,29 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // EditText의 참조값 얻어와서 필드에 저장하기
+        editText=findViewById(R.id.editText);
+        // Button의 참조값 얻어와서 리스너 등록하기
+        Button saveBtn=findViewById(R.id.saveBtn);
+        saveBtn.setOnClickListener(this);
+        // Switch의 참조값을 필드에 저장하기
+        sw=findViewById(R.id.switch1);
+        // Switch의 체크 상태가 바뀌었음을 감지할 리스너 등록
+        sw.setOnCheckedChangeListener(this);
+
+        SharedPreferences pref=getSharedPreferences("info", MODE_PRIVATE);
+        // .getString(key값, default값)
+        String myName=pref.getString("name", "empty");
+        if(myName.equals("empty")){
+            return;
+        }
+        // 저장된 이름을 editText에 출력하기
+        editText.setText(myName);
+        // Switch 체크 여부 읽어오기
+        boolean isChecked=pref.getBoolean("isChecked", false);
+        // Switch의 체크 상태 반영하기
+        sw.setChecked(isChecked);
     }
 
     @Override
@@ -58,7 +93,40 @@ public class MainActivity extends AppCompatActivity {
         }else if(id==R.id.start){
 
         }
-
         return true;
+    }
+
+    // 버튼을 눌렀을 때 실행순서는 여기로 들어오게 된다.
+    @Override
+    public void onClick(View view) {
+        // 입력한 문자열 읽어오기
+        String inputName=editText.getText().toString();
+        // Switch 체크상태 읽어오기
+        boolean isChecked=sw.isChecked();
+
+        // SharedPreferences를 활용해서 저장하기
+        SharedPreferences pref=getSharedPreferences("info", MODE_PRIVATE);
+        // Editor 객체의 참조값을 얻어와서
+        SharedPreferences.Editor editor=pref.edit();
+        // myName이라는 키값으로 문자열 저장하기
+        editor.putString("myName", inputName);
+        editor.putBoolean("isChecked", isChecked);
+        // 실제 저장되는 시점
+        editor.commit();
+        // 알림 띄우기
+        new AlertDialog
+                .Builder(this)
+                .setMessage("Successfully Saved.")
+                .setNeutralButton("OK", null)
+                .show();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if(b){
+            sw.setText("ON");
+        }else{
+            sw.setText("OFF");
+        }
     }
 }
--- step12sharedpref/src/main/java/com/winnie/step12sharedpref/SettingsActivity.java
+++ step12sharedpref/src/main/java/com/winnie/step12sharedpref/SettingsActivity.java
@@ -11,20 +11,29 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // res/layout/settings_activity.xml 문서를 전개해서 화면 구성하기
         setContentView(R.layout.settings_activity);
+        /*
+            id가 settings인 레이아웃 (FrameLayout)에
+            SettingsFragment로 화면 구성하게 하기
+         */
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment())
                 .commit();
+        // 좌상단 up 버튼 동작 가능하도록 한다.
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
 
+    // FrameLayout을 채울 Fragment
     public static class SettingsFragment extends PreferenceFragmentCompat {
+        // Fragment가 처음 활성화 될 때 호출되는 메소드
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            // res/xml/root_preferences.xml 문서를 전개해서 Fragment UI 구성하기
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
     }
